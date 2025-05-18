62af1bc3c7bcd9cceb04e59297bc355ba9b90851
Jacky Wong <jackywong8525@gmail.com>
2025-05-18 15:45:03 +0800

    Refine app ui

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ let web3;
 let contract;
 let userAccount;
 let connected = false;
+let isTransactionLoading = false;
 
 async function connectWallet() {
     if (window.ethereum && loaded) {
@@ -19,10 +20,15 @@ async function connectWallet() {
         await window.ethereum.request({ method: "eth_requestAccounts" });
         userAccount = (await web3.eth.getAccounts())[0];
         contract = new web3.eth.Contract(contractABI, contractAddress);
-        alert(`Connected: ${userAccount} + ${ contract}`);
-        console.log(contract.methods);
+        alert(`Connected to the address: ${userAccount}`);
         connected = true;
         document.querySelector('.contract-operation-container').style.display = "flex";
+
+        const connectButton = document.querySelector('.connect-button');
+        
+        connectButton.innerHTML = 'Connected!';
+        connectButton.disabled = true;
+
 
         await getCollateralBalance();
         await getLoanBalance();
@@ -33,6 +39,8 @@ async function connectWallet() {
 
 async function deposit() {
     try {
+        toggleLoading();
+
         const depositAmount = document.querySelector('.deposit-input').value;
         await contract.methods.deposit().send({
             from: userAccount, 
@@ -41,39 +49,66 @@ async function deposit() {
         alert("Deposited!");
         document.querySelector('.deposit-input').value = 0;
         await getCollateralBalance();
+
+        toggleLoading();
     } catch(error){
         console.log(error.message);
+        toggleLoading();
     }
 }
 
 async function withdraw() {
-    const withdrawAmount = document.querySelector('.withdraw-input').value;
-    await contract.methods.withdraw(web3.utils.toWei(withdrawAmount, "ether")).send({
-         from: userAccount 
-    });
-    alert("Withdrew!");
-    document.querySelector('.withdraw-input').value = 0;
-    await getCollateralBalance();
+    try{
+        toggleLoading();
+
+        const withdrawAmount = document.querySelector('.withdraw-input').value;
+        await contract.methods.withdraw(web3.utils.toWei(withdrawAmount, "ether")).send({
+            from: userAccount 
+        });
+        alert("Withdrew!");
+        document.querySelector('.withdraw-input').value = 0;
+        await getCollateralBalance();
+
+        toggleLoading();
+    } catch(error){
+        toggleLoading();
+    }
+    
 }
 
 async function borrow() {
-    const borrowAmount = document.querySelector('.borrow-input').value;
+    try{
+        toggleLoading();
+        const borrowAmount = document.querySelector('.borrow-input').value;
 
-    await contract.methods.borrow(web3.utils.toWei(borrowAmount, "ether")).send({ from: userAccount });
-    alert("Borrowed!");
-    document.querySelector('.borrow-input').value = 0;
-    await getLoanBalance();
+        await contract.methods.borrow(web3.utils.toWei(borrowAmount, "ether")).send({ from: userAccount });
+        alert("Borrowed!");
+        document.querySelector('.borrow-input').value = 0;
+        await getLoanBalance();
+
+        toggleLoading();
+    } catch(error){
+        toggleLoading();
+    }
+    
 }
 
 async function repay() {
-    const repayAmount = document.querySelector('.repay-input').value;
-    await contract.methods.repay().send({ 
-        from: userAccount, 
-        value: web3.utils.toWei(repayAmount, "ether") 
-    });
-    alert("Repaid Loan!");
-    document.querySelector('.repay-input').value = 0;
-    await getLoanBalance();
+    try{
+        toggleLoading();
+        const repayAmount = document.querySelector('.repay-input').value;
+        await contract.methods.repay().send({ 
+            from: userAccount, 
+            value: web3.utils.toWei(repayAmount, "ether") 
+        });
+        alert("Repaid Loan!");
+        document.querySelector('.repay-input').value = 0;
+        await getLoanBalance();
+        toggleLoading();
+    } catch(error){
+        toggleLoading();
+    }
+    
 }
 
 async function getCollateralBalance(){
@@ -94,4 +129,20 @@ async function getLoanBalance() {
         from: userAccount
     });
     document.querySelector('.loan-balance').innerHTML = web3.utils.fromWei(balance, "ether");
+}
+
+function toggleLoading(){
+    isTransactionLoading = !isTransactionLoading;
+
+    if(isTransactionLoading){
+        document.querySelector('.app-container').inert = true;
+        document.querySelector('.app-container').style.webkitFilter = "brightness(20%)";
+        document.querySelector('.message-container').style.display = "flex";
+    }
+
+    else{
+        document.querySelector('.app-container').inert = false;
+        document.querySelector('.app-container').style.webkitFilter = "none";
+        document.querySelector('.message-container').style.display = "none";
+    }
 }
